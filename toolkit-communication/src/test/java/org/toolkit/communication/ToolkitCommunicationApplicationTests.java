16ad56c5a8c83bc3ba6b5ff5168869f956619dd1
lihanyun <hanyun.li@fittime.com>
2021-11-18 16:11:06 +0800

    补充单元测试用例

--- toolkit-communication/src/test/java/org/toolkit/communication/ToolkitCommunicationApplicationTests.java
+++ toolkit-communication/src/test/java/org/toolkit/communication/ToolkitCommunicationApplicationTests.java
@@ -5,19 +5,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.toolkit.communication.email.util.EmailUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class ToolkitCommunicationApplicationTests {
 
     @Test
     void contextLoads() {
+
+        EmailUtil.sendMail("test@qq.com", "test一下", "测试发送");
+
+        List<String> receivingAddresses = new ArrayList<>(2);
+        receivingAddresses.add("test1@qq.com");
+        receivingAddresses.add("test2@qq.com");
+        EmailUtil.sendMail(receivingAddresses, "test一下", "测试发送");
+
+
+        Map<String, String> receivingAddressesAndEmailTitle = new HashMap<>(2);
+        receivingAddressesAndEmailTitle.put("test1@qq.com", "测试发送");
+        receivingAddressesAndEmailTitle.put("test2@qq.com", "测试发送");
+        EmailUtil.sendMail(receivingAddressesAndEmailTitle, "test一下");
+
+
         // 测试使用sendMailParam作为入参数时的单次发送情况
         EmailUtil.SendMailParam sendMailParam = EmailUtil.builderSingleSendMailParam();
         sendMailParam.setReceivingAddress("test@qq.com");
         sendMailParam.setEmailContent("test一下");
         sendMailParam.setEmailTitle("测试发送");
         EmailUtil.sendMail(sendMailParam);
+
 
         // 测试使用sendMailParam作为入参数时的批量发送情况
         List<EmailUtil.SendMailParam> sendMailParams = new ArrayList<>();
